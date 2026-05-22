230d5026ec345e713d41f5f4e8f5080512c80881
leslie628 <lf24aap@herts.ac.uk>
2026-05-22 20:55:54 +0100

    added task web app (deployed) origin to policy

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins(["http://localhost:5173", "https://task-web-app-amber.vercel.app"])
                   .AllowAnyMethod()
                   .AllowAnyHeader()
                   .AllowCredentials();
